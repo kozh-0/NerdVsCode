34816e059e8be897c5608fa1065f2619716f240a
Александр Гладун <alexandr20021995@gmail.com>
2023-08-29 23:01:43 +0600

    save order in database

---
+++ backend/src/main/java/com/example/dto/request/OrderDtoRequest.java
@@ -0,0 +1,24 @@
+package com.example.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDtoRequest {
+
+    private String name;
+    @NotEmpty(message = "{passportdetails.notempty}")
+    private String passportDetails;
+    private double monthlyPayment;
+    private double rate;
+    private double initFee;
+    private int year;
+    private String email;
+    private String telegram;
+
+}
---
+++ backend/src/main/java/com/example/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Client;
+import com.example.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
