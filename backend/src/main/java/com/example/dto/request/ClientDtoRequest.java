cc64b9c52ec3567df0d974761b27eaa565b3d70c
Александр Гладун <alexandr20021995@gmail.com>
2023-08-29 19:30:47 +0600

    validation and documentation

--- backend/src/main/java/com/example/dto/request/ClientDtoRequest.java
+++ backend/src/main/java/com/example/dto/request/ClientDtoRequest.java
@@ -4,16 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientDtoRequest {
 
+    @NotEmpty(message = "{lastname.notempty}")
+    @Size(max = 25, message = "{lastname.size}")
+    @Pattern(regexp = "^(([А-Я][а-яё]+)|([A-Z][a-z]+))$", message = "{lastname.pattern}")
     private String lastName;
+    @NotEmpty(message = "{firstname.notempty}")
+    @Size(max = 25, message = "{firstname.size}")
+    @Pattern(regexp = "^(([А-Я][а-яё]+)|([A-Z][a-z]+))$", message = "{firstname.pattern}")
     private String firstName;
+    @Size(max = 25, message = "{patronymic.size}")
+    @Pattern(regexp = "^(([А-Я][а-яё]+)|([A-Z][a-z]+))$", message = "{patronymic.pattern}")
     private String patronymic;
+    @NotEmpty(message = "{passportdetails.notempty}")
     private String passportDetails;
+    @NotEmpty(message = "{email.notempty}")
+    @Email(message = "{email.email}")
     private String email;
     private String telegram;
 
---
+++ backend/src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ServerException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleServerException(ServerException ex) {
+        return new ResponseEntity<>(ex.getErrorCode().getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
